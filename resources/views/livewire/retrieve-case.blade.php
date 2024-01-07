5762b08c751ff46a56279fdfba1d073f68fac1d1
Ng Soo Hao <ngsoohao1@gmail.com>
2024-01-08 03:16:50 +0800

    fixed bug

--- resources/views/livewire/retrieve-case.blade.php
+++ resources/views/livewire/retrieve-case.blade.php
@@ -10,7 +10,7 @@
                 <th class="p-4 m-2 text-white border border-gray-300 bg-slate-600">Feedback</th>
                 <th class="p-4 m-2 text-white border border-gray-300 bg-slate-600">Description</th>
                 <th class="p-4 m-2 text-white border border-gray-300 bg-slate-600">Status</th>
-                <th class="p-4 m-2 text-white border border-gray-300 bg-slate-600">Action</th>
+                {{-- <th class="p-4 m-2 text-white border border-gray-300 bg-slate-600">Action</th> --}}
 
             </tr>
         </thead>
@@ -24,7 +24,7 @@
                     <td class="border border-slate-600" >{{ $case->feedback }}</td>
                     <td class="max-w-xs break-words border border-slate-600">{{ $case->description }}</td>
                     <td class="border border-slate-600">{{ $case->status }}</td>
-                    <td class="py-4 border border-slate-600"><a class="px-4 py-2 text-white rounded-md bg-slate-700 hover:bg-slate-400"href="">View More Details</a></td>
+                    {{-- <td class="py-4 border border-slate-600"><a class="px-4 py-2 text-white rounded-md bg-slate-700 hover:bg-slate-400"href="">View More Details</a></td> --}}
 
                 </tr>
             @endforeach
